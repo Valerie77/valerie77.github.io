8e53e788904485043598969f0127c8dbfd9e30ff
Valerie77 <georgieva237@gmail.com>
2022-03-16 16:13:10 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,4 @@ var element = document.getElementById("test");
 function changeAttr() {
   element.SetAttribute("value","Submit is changed");
 }
-element.addeventListener("click", changeAttr);
+element.addEventListener("click", changeAttr);
